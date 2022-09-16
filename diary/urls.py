f2f82704a9a189d3e3bc5b036946840865d94ae7
yuutaroudesuga5615 <ykh2135106@stu.o-hara.ac.jp>
2022-09-16 11:06:17 +0900

    916日 削除機能 追加

--- diary/urls.py
+++ diary/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('diary-detail/<int:pk>/', views.DiaryDetailView.as_view(), name = "diary_detail"),
     path('diary-create/', views.DiaryCreateView.as_view(), name = "diary_create"),
     path('diary-update/<int:pk>/', views.DiaryUpdateView.as_view(), name = "diary_update"),
+    path('diary-delete/<int:pk>/', views.DiaryDeleteView.as_view(), name = "diary_delete"),
 ]
 
 

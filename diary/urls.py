be35fd45dbb429d5b5085df70508035ee72c6868
yuutaroudesuga5615 <ykh2135106@stu.o-hara.ac.jp>
2022-09-16 10:44:13 +0900

    916日 変更機能追加

--- diary/urls.py
+++ diary/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     path('inquiry/', views.InquiryView.as_view(), name = "inquiry"),
     path('diary-list/', views.DiaryListView.as_view(), name = "diary_list"),
     path('diary-detail/<int:pk>/', views.DiaryDetailView.as_view(), name = "diary_detail"),
-    path('diary-create/', views.DiaryCreateView.as_view(), name = "diary_create")
+    path('diary-create/', views.DiaryCreateView.as_view(), name = "diary_create"),
+    path('diary-update/<int:pk>/', views.DiaryUpdateView.as_view(), name = "diary_update"),
 ]
 
 
